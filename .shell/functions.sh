7012116f917502704526c63ac00c4fde13d63d32
gukele <805926222@qq.com>
2024-09-28 17:11:36 +0800

    [fix] 增加了更安全的修改环境变量函数

--- .shell/functions.sh
+++ .shell/functions.sh
@@ -8,6 +8,7 @@ has_cmd() {
     return 0
 }
 
+# =============================================================
 path_remove() {
     PATH=$(echo -n "$PATH" | awk -v RS=: -v ORS=: "\$0 != \"$1\"" | sed 's/:$//')
 }
@@ -21,6 +22,42 @@ path_prepend() {
     path_remove "$1"
     PATH="$1${PATH:+":$PATH"}"
 }
+
+# ==================================================================
+# 从指定变量中移除某个条目
+var_remove() {
+    local var_name="$1"
+    local value_to_remove="$2"
+    
+    eval "$var_name=\$(echo -n \${$var_name} | awk -v RS='[ :]' -v ORS=' ' '\$0 != \"$value_to_remove\"' | sed 's/[ :]\+$//; s/^[ :]\+//')"
+    export "$var_name"
+}
+
+# 向指定变量的末尾追加多条目
+var_append() {
+    local var_name="$1"
+    shift  # 移除第一个参数，后面的参数是要追加的值
+
+    for value_to_append in "$@"; do
+        var_remove "$var_name" "$value_to_append"  # 先移除已存在的值
+        eval "$var_name=\"\${$var_name} $value_to_append\""
+    done
+    export "$var_name"
+}
+
+# 向指定变量的开头追加多条目
+var_prepend() {
+    local var_name="$1"
+    shift  # 移除第一个参数，后面的参数是要追加的值
+
+    for value_to_prepend in "$@"; do
+        var_remove "$var_name" "$value_to_prepend"  # 先移除已存在的值
+        eval "$var_name=\"$value_to_prepend \${$var_name}\""
+    done
+    export "$var_name"
+}
+
+# ==================================================================
 
 here() {
     local loc
